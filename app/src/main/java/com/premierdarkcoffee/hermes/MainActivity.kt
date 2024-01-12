c716ae4ea29753102231358343c4061f4583a922
ldgomm <ruiz211@protonmail.com>
2024-01-12 11:39:52 -0500

    Project initialisation

--- app/src/main/java/com/premierdarkcoffee/hermes/MainActivity.kt
+++ app/src/main/java/com/premierdarkcoffee/hermes/MainActivity.kt
@@ -17,24 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HermesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("World")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HermesTheme {
-        Greeting("Android")
     }
 }
