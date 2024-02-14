598aa0c24c865bb39af6ae9aecd980034f9541dc
ldgomm <ruiz211@protonmail.com>
2024-02-14 16:50:29 -0500

    Working on models

---
+++ app/src/main/java/com/premierdarkcoffee/hermes/app/features/sales/data/remote/model/sales/Photo.kt
@@ -0,0 +1,6 @@
+package com.premierdarkcoffee.hermes.app.features.sales.data.remote.model.sales
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Photo(var url: String)
--- app/src/main/java/com/premierdarkcoffee/hermes/features/sales_feature/data/model/sales/Price.kt
+++
@@ -1,8 +0,0 @@
-package com.premierdarkcoffee.hermes.features.sales_feature.data.model.sales
-
-import com.premierdarkcoffee.hermes.features.sales_feature.data.model.sales.CreditCard
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Price(var cash: Double, var affiliate: Double? = null, var creditCard: CreditCard? = null)
-
